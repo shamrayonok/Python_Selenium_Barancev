965c36c3a3afa9da7413279b1167709a900a8579
shamrayonok <33039157+shamrayonok@users.noreply.github.com>
2018-11-13 23:16:25 +0200

    Добавлены assert  в Del Group

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -3,7 +3,11 @@ from model.group import Group
 import time
 
 def test_delete_first_groupe(app):
+    old_groups = app.group.get_group_list()
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
-    time.sleep(2)
     app.group.delete_first_group()
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) - 1 == len(new_groups)
+    old_groups[0:1] = []
+    assert old_groups == new_groups
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -3,9 +3,16 @@ from model.group import Group
 
 
 def test_modify_group_name(app):
+    old_groups = app.group.get_group_list()
     app.group.modify_first_group(Group(name="New Group"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
+
 
 def test_modify_group_header(app):
+    old_groups = app.group.get_group_list()
     app.group.modify_first_group(Group(header="New Header"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
