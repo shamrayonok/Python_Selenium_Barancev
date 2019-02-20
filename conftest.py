43d17fc69ebef1c07721a9e14808d7576e9a2a09
shamrayonok <33039157+shamrayonok@users.noreply.github.com>
2019-02-21 00:39:54 +0200

    По 31 урок, включительно

--- conftest.py
+++ conftest.py
@@ -1,6 +1,7 @@
 import pytest
 import json
 import os.path
+import importlib
 from fixture.application import Application
 
 
@@ -37,4 +38,11 @@ def pytest_addoption(parser):
     parser.addoption("--target", action="store", default="target.json")
 
 
-print(os.path.dirname(os.path.abspath(__file__)))
+def pytest_generate_tests(metafunc):
+    for fixture in metafunc.fixturenames:
+        if fixture.startswith("data_"):
+            testdata = load_from_module(fixture[5:])
+            metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
+
+def load_from_module(module):
+    return importlib.import_module("data.%s" % module).testdata
