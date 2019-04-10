303909327953608eeb54b4b5630e79163f9ef3ee
shamrayonok <33039157+shamrayonok@users.noreply.github.com>
2019-04-10 23:28:05 +0300

    по 37 включительно

--- fixture/db.py
+++ fixture/db.py
@@ -18,7 +18,7 @@ class DbFixture:
             cursor.execute("select group_id, group_name, group_header, group_footer from group_list")
             for row in cursor:
                 (id, name, header, footer) = row
-                list.append(Group(id=str(id), name=name, header=header, footer=footer))
+                list.append(Group(id=str(id), name=name.strip(), header=header, footer=footer))
         finally:
             cursor.close()
         return list
